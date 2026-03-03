0cfc0c5ca2219a6998a1dcd1348c345aa9c23cad
Cédric Belin <cedx@outlook.com>
2026-03-03 17:58:19 +0100

    Add the `New-DetailsElement` cmdlet

---
+++ test/Elements/New-DetailsElement.Tests.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+	Tests the features of the `New-DetailsElement` cmdlet.
+#>
+Describe "New-DetailsElement" {
+	BeforeAll {
+		Import-Module "$PSScriptRoot/../../Html.psd1"
+	}
+
+	It 'should support the "name" attribute' {
+		details -name MyGroup | Should -BeExactly '<details name="MyGroup"></details>'
+	}
+
+	It 'should support the "open" attribute' {
+		details -open | Should -BeExactly '<details open></details>'
+	}
+}
