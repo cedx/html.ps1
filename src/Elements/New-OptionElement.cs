a7a16deb275be4f1815e7b2df3648cdeaa38f8fd
Cédric Belin <cedx@outlook.com>
2026-03-03 23:48:44 +0100

    Add the `New-OptionElement` cmdlet

---
+++ src/Elements/New-OptionElement.cs
@@ -0,0 +1,44 @@
+namespace Belin.Html.Cmdlets.Elements;
+
+/// <summary>
+/// Creates a new <c>option</c> element.
+/// </summary>
+[Cmdlet(VerbsCommon.New, "HtmlOptionElement"), Alias("option"), OutputType(typeof(string))]
+public class NewOptionElementCommand(): NewElementCommand("option", isVoid: false) {
+
+	/// <summary>
+	/// Value indicating whether the option is not checkable.
+	/// </summary>
+	[Parameter(ValueFromPipelineByPropertyName = true)]
+	public SwitchParameter Disabled { get; set; }
+
+	/// <summary>
+	/// The label indicating the meaning of the option.
+	/// </summary>
+	[Parameter(ValueFromPipelineByPropertyName = true)]
+	public string Label { get; set; } = "";
+
+	/// <summary>
+	/// Value indicating whether the option is initially selected.
+	/// </summary>
+	[Parameter(ValueFromPipelineByPropertyName = true)]
+	public SwitchParameter Selected { get; set; }
+
+	/// <summary>
+	/// The value to be submitted with the form.
+	/// </summary>
+	[Parameter(ValueFromPipelineByPropertyName = true)]
+	public string? Value { get; set; }
+
+	/// <summary>
+	/// Populates the specified attribute collection with the element attributes.
+	/// </summary>
+	/// <param name="attributes">The attribute collection to populate.</param>
+	protected override void RenderAttributes(Dictionary<string, object?> attributes) {
+		base.RenderAttributes(attributes);
+		if (Disabled) attributes["disabled"] = true;
+		if (!string.IsNullOrWhiteSpace(Label)) attributes["label"] = Label;
+		if (Selected) attributes["selected"] = true;
+		if (Value is not null) attributes["value"] = Value;
+	}
+}
---
+++ test/Elements/New-OptionElement.Tests.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+	Tests the features of the `New-OptionElement` cmdlet.
+#>
+Describe "New-OptionElement" {
+	BeforeAll {
+		Import-Module "$PSScriptRoot/../../Html.psd1"
+	}
+
+	It 'should support the "disabled" and "selected" attributes' {
+		option -disabled | Should -BeExactly "<option disabled></option>"
+		option -selected | Should -BeExactly "<option selected></option>"
+	}
+
+	It 'should support the "label" and "value" attributes' -ForEach @(
+		@{ Label = "Option #1"; Value = "" }
+		@{ Label = "Option #2"; Value = "FooBar" }
+	) {
+		option -label $label -value $value | Should -BeIn "<option label=`"$label`" value=`"$value`"></option>", "<option value=`"$value`" label=`"$label`"></option>"
+	}
+}
