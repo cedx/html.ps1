1a15a6facd62d6f5c446e587aa454d98dc1ed4e1
Cédric Belin <cedx@outlook.com>
2026-04-13 22:41:30 +0200

    Code formatting [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,5 @@
-"Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
+
+"Performing the static analysis of source code..."
 $PSScriptRoot, "test" | Invoke-ScriptAnalyzer -Recurse
 Test-ModuleManifest Html.psd1 | Out-Null
