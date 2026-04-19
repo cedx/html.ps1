6b46f92e4f9962b9cd1dcc5b989c05645349a3b1
Cédric Belin <cedx@outlook.com>
2026-04-19 19:09:18 +0200

    Rename the examples

--- example/Out-File.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-	Renders an HTML document from the given PowerShell script.
-#>
-Import-Module Belin.Html
-
-$title = "My Application"
-$content = & "$PSScriptRoot/../res/Content.ps1" -Heading $title | Out-String
-& "$PSScriptRoot/../res/Layout.ps1" $content -Title $title | Out-File index.html
