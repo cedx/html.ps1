528ab2e33dad156a7cd5416a93491062f45bfa4a
Cédric Belin <cedx@outlook.com>
2026-04-23 23:56:44 +0200

    Add the `Format-KebabCaseLower` cmdlet

---
+++ src/Format-KebabCaseLower.psm1
@@ -0,0 +1,24 @@
+using namespace System.Text.Json
+
+<#
+.SYNOPSIS
+	Converts the specified name according to lowercase kebab-casing.
+.INPUTS
+	The name to convert.
+.OUTPUTS
+	The converted name.
+#>
+function Format-KebabCaseLower {
+	[CmdletBinding()]
+	[OutputType([string])]
+	param (
+		# The name to convert.
+		[Parameter(Mandatory, Position = 0)]
+		[AllowEmptyString()]
+		[string] $InputObject
+	)
+
+	process {
+		[JsonNamingPolicy]::KebabCaseLower.ConvertName($InputObject)
+	}
+}
